53c29461bb130826bcbee9e0d371473d4188ae64
rybachello <mykolarybachello@gmail.com>
2020-10-25 20:36:55 +0200

    remove gitignore files

--- ios/Pods/Target Support Files/Pods-transitionExample-transitionExampleTests/Pods-transitionExample-transitionExampleTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_transitionExample_transitionExampleTests : NSObject
-@end
-@implementation PodsDummy_Pods_transitionExample_transitionExampleTests
-@end
